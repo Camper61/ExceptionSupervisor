afdf81738a860a22e4ebec509755fe8ac8af73ad
Louka <22691132+LoukaMB@users.noreply.github.com>
2018-05-21 08:22:45 -0400

    Create SupervisorExample.cpp

---
+++ SupervisorExample/SupervisorExample.cpp
@@ -0,0 +1,34 @@
+
+#include "stdafx.h"
+
+LONG WINAPI ExampleSupervisor(PEXCEPTION_RECORD pExceptionRecord, PCONTEXT pContext)
+{
+	std::cout << "[#] Caught exception (" << std::hex << pExceptionRecord->ExceptionCode << ") at EIP=0x" << std::hex << pContext->Eip << std::endl;
+	pContext->Eip += 10; /* skip over instruction to prevent error rethrow */
+	return EXCEPTION_CONTINUE_EXECUTION;
+}
+
+int main()
+{
+	std::cout << "[#] Applying ExceptionSupervisor... ";
+	AddSupervisorExceptionHandler(ExampleSupervisor);
+	if (!EnableExceptionSupervisor())
+	{
+		std::cout << "FAILURE";
+		return -1;
+	}
+	std::cout << "SUCCESS\n";
+	__try
+	{
+		volatile int* px = NULL;
+		*px = 'err';
+	}
+	__except (GetExceptionInformation(), GetExceptionCode())
+	{
+		/* This should never get executed, as our supervisor returns EXCEPTION_CONTINUE_EXECUTION */
+		std::cout << "Our exception was caught, this should not happen!\n";
+		return -1;
+	}
+    return 0;
+}
+
